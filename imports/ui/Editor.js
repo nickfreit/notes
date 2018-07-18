6a4d926f143f870c0f210a5d9b802ffb1a782eb3
Nick Freiter <nickfreit@gmail.com>
2018-07-18 08:34:57 -0400

    Modifying layout for page content

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -41,7 +41,7 @@ export class Editor extends React.Component {
   render() {
     if (this.props.note) {
       return (
-        <div>
+        <div className='editor'>
           <input
             value={this.state.title}
             placeholder="Untitled note"
@@ -59,11 +59,13 @@ export class Editor extends React.Component {
       );
     } else {
       return (
-        <p>
-          {this.props.selectedNoteId ?
-            'Note not found' :
-            'Pick or create a note to get started'}
-        </p>
+        <div className='editor'>
+          <p>
+            {this.props.selectedNoteId ?
+              'Note not found' :
+              'Pick or create a note to get started'}
+          </p>
+        </div>
       );
     }
   }
